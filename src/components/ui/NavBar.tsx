46488b76bc50810245190ac3e6d0509e5a98d88a
András Szücs <andrew0szucs@gmail.com>
2023-10-07 18:57:17 +0200

    feat/add-logo

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,7 +1,6 @@
-import { PersonIcon } from "@radix-ui/react-icons";
 import React from "react";
-import { Button } from "./Button";
 import Large from "./Typography/Large";
+import Image from "next/image";
 
 interface Props {
   button: React.ReactNode;
@@ -9,8 +8,16 @@ interface Props {
 
 export default function NavBar({ button }: Props) {
   return (
-    <nav className="h-12 px-4 flex items-center justify-center">
-      <div className="flex-1" />
+    <nav className="h-20 px-4 mb-4 flex items-center justify-center">
+      <div className="flex-1">
+        <Image
+          className="border-1 border-slate-800 rounded-md"
+          src="/images/logo.jpeg" // Route of the image file
+          height={50} // Desired size with correct aspect ratio
+          width={50} // Desired size with correct aspect ratio
+          alt="ZkToro Logo"
+        />
+      </div>
       <div className="flex-1">
         <Large>zkToro</Large>
       </div>
