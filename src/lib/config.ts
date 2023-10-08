a4848a882ec5a0b459771741eb413857679f59e5
András Szücs <andrew0szucs@gmail.com>
2023-10-08 11:40:07 +0200

    chore/map-network-conf-to-env

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,12 +1,13 @@
 import { ethers } from "ethers";
 
-const rpcUrl = "https://rpc-mumbai.maticvigil.com/";
-const chainId = 80001;
-const registry = "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b";
+const rpcUrl = process.env.NEXT_PUBLIC_NETWORK_RPC_URL;
+const chainId = process.env.NEXT_PUBLIC_CHAIN_ID;
+const registry = process.env.NEXT_PUBLIC_REGISTRY_CONTRACT_ADDRESS;
+const name = process.env.NEXT_PUBLIC_NETWORK_NAME;
 
 export const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 export const ethrProvider = {
-  name: `maticmum`,
+  name,
   chainId,
   rpcUrl,
   registry,
