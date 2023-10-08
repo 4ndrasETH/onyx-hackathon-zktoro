bed612c41384e02389a2f0dc350551937519b559
luchenghao <lu.chenghao24@gmail.com>
2023-10-08 23:14:15 +0800

    add header

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    async headers() {
+      return [
+        {
+          source: '/api/vp',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'no-store', // Matched parameters can be used in the value
+            },
+          ],
+        },
+      ]
+    },
+  }
