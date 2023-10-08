7b3db6ef6e0b015a3050fcd73032f68846187ca8
András Szücs <andrew0szucs@gmail.com>
2023-10-08 12:11:46 +0200

    fix: cast env vars to string

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,9 +1,9 @@
 import { ethers } from "ethers";
 
-const rpcUrl = process.env.NEXT_PUBLIC_NETWORK_RPC_URL;
-const chainId = process.env.NEXT_PUBLIC_CHAIN_ID;
-const registry = process.env.NEXT_PUBLIC_REGISTRY_CONTRACT_ADDRESS;
-const name = process.env.NEXT_PUBLIC_NETWORK_NAME;
+const rpcUrl = process.env.NEXT_PUBLIC_NETWORK_RPC_URL as string;
+const chainId = process.env.NEXT_PUBLIC_CHAIN_ID as string;
+const registry = process.env.NEXT_PUBLIC_REGISTRY_CONTRACT_ADDRESS as string;
+const name = process.env.NEXT_PUBLIC_NETWORK_NAME as string;
 
 export const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 export const ethrProvider = {
